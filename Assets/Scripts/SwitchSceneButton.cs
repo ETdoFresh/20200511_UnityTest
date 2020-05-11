2c53c43b8022f458ce448cdcf7b4020c13d5229f
ETdoFresh <ETdoFresh@gmail.com>
2020-05-11 17:18:39 -0500

    Initial Commit

---
+++ Assets/Scripts/SwitchSceneButton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class SwitchSceneButton : MonoBehaviour
+{
+    public Button button;
+    public string sceneName;
+
+    private void OnValidate()
+    {
+        if (!button) button = GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+#if UNITY_EDITOR
+        UnityEditor.Events.UnityEventTools.AddPersistentListener(button.onClick, LoadScene);
+#else
+        button.onClick.AddListener(LoadScene);
+#endif
+    }
+
+    private void OnDisable()
+    {
+#if UNITY_EDITOR
+        UnityEditor.Events.UnityEventTools.RemovePersistentListener(button.onClick, LoadScene);
+#else
+        button.onClick.RemoveListener(LoadScene);
+#endif
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
